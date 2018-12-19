0c4fb288d834ff75e904eb579ae3a0148cd57e1c
Ciro Souza <ciro.souza@zoly.com.br>
2018-12-18 22:10:07 -0200

    Initial Commit

---
+++ FirebaseTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTestApp
+//
+//  Created by Ciro Souza on 18/12/18.
+//  Copyright © 2018 Zoly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
